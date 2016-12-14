063b26d60fcb8ecd01c8f04859804f0ad820df41
juanfra <juanframnz@gmail.com>
2016-12-14 23:53:55 +0100

    Subida primer estilo

--- gastoscontrolados/views.py
+++ gastoscontrolados/views.py
@@ -1,10 +1,11 @@
 from django.db.models import Q
 from django.shortcuts import render
-from forms import IngresosForm
-from forms import GastosForm
+from .forms import IngresosForm
+from .forms import GastosForm
 from django.template import RequestContext
 from .models import Ingresos
 from .models import Gastos
+from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the gastos index.")
@@ -16,15 +17,21 @@ def vingresos(request):
     ingresos_all = Ingresos.objects.order_by('fecha')
     gastos_all = Gastos.objects.order_by('fecha')
     if request.method == 'POST':
-        formIng = IngresosForm(request.POST)
-        formGas = GastosForm(request.POST)
-        if formIng.is_valid():
-            formIng.save()
-            formIng = IngresosForm()
-            formGas.save()
-            formGas = GastosForm()
-            return render(request, 'gastoscontrolados/ingreso.html', {'formIng': formIng,'formGas': formGas,'ingresos': ingresos_all,'gastos': gastos_all})    
-    else:
-        formIng = IngresosForm()
-        formGas = GastosForm()
+        if 'ing_save' in request.POST:
+            formIng = IngresosForm(request.POST)
+            if formIng.is_valid():
+                formIng.save()
+                formIng = IngresosForm()
+                formGas = GastosForm()
+                return render(request, 'gastoscontrolados/ingreso.html', {'formIng': formIng,'formGas': formGas,'ingresos': ingresos_all,'gastos': gastos_all})    
+        if 'gastos_save' in request.POST:
+            formGas = GastosForm(request.POST)
+            if formGas.is_valid():
+                formGas.save()
+                formIng = IngresosForm()
+                formGas = GastosForm()
+                return render(request, 'gastoscontrolados/ingreso.html', {'formIng': formIng,'formGas': formGas,'ingresos': ingresos_all,'gastos': gastos_all})    
+    formIng = IngresosForm()
+    formGas = GastosForm()
     return render(request, 'gastoscontrolados/ingreso.html', {'formIng': formIng,'formGas': formGas,'ingresos': ingresos_all,'gastos': gastos_all})
+        
--- proyectos/urls.py
+++ proyectos/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^gastoscontrolados/', include('gastoscontrolados.urls')),
+    url(r'^', include('gastoscontrolados.urls')),
     url(r'^admin/', admin.site.urls),
 ]
